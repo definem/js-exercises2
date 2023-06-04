eb8c173ad2ff6d3cd14ce054706c274aeb04cc78
define) <120991965+definem@users.noreply.github.com>
2023-06-05 03:32:08 +0500

    added

---
+++ exercises.js
@@ -0,0 +1,15 @@
+// <---- Bismillah ---->
+
+// Exercises from CodingJs
+
+// <---- Exercise-1 sleepIn ---->
+
+// function sleepIn(weekday, vacation) {
+//   if (weekday == true && vacation == false) {
+//     return false;
+//   } else {
+//     return true;
+//   }
+// }
+
+// <---- Exercise-2 monkeyTrouble ---->
