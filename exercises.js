eebe6687648ff1f2713667b9924577ac23e8abc9
define) <120991965+definem@users.noreply.github.com>
2023-06-05 11:29:12 +0500

    exercise added

--- exercises.js
+++ exercises.js
@@ -13,3 +13,13 @@
 // }
 
 // <---- Exercise-2 monkeyTrouble ---->
+
+// function monkeyTrouble(aSmile, bSmile) {
+//   if (aSmile == true && bSmile == true) {
+//     return true;
+//   } else if (aSmile == false && bSmile == false) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
