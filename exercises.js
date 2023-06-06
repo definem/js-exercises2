c8d0af36dd2bb905e1cfaedd4091ed99cf09fe1b
define) <120991965+definem@users.noreply.github.com>
2023-06-07 02:34:20 +0500

    ex 18

--- exercises.js
+++ exercises.js
@@ -131,3 +131,9 @@
 //     return false;
 //   }
 // }
+
+// <---- Exercise-17  icyHot----> 
+
+// function icyHot(temp1, temp2) {
+//   return (temp1 < 0 && temp2 > 100) || (temp1 > 100 && temp2 < 0);
+// }
