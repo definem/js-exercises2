10cccd4c7da563c3527095340ee9de51f8ce4d3f
define) <120991965+definem@users.noreply.github.com>
2023-06-06 09:21:48 +0500

    added

--- exercises.js
+++ exercises.js
@@ -113,7 +113,7 @@
 
 // <---- Exercise-13 ---->
 
-function or35(n) {
-  if (n % 3 == 0 || n % 5 == 0) return true;
-  return false;
-}
+// function or35(n) {
+//   if (n % 3 == 0 || n % 5 == 0) return true;
+//   return false;
+// }
